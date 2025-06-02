9af5171b1f2acc609232b08084580b1d20ba8f9b
DevSaini4752 <devsaini4752@gmail.com>
2025-06-02 17:02:49 +0530

    Completed weather module: Module complete; historical/forecast disabled by API

--- weather.py
+++ weather.py
@@ -6,49 +6,28 @@ The function will take the following arguments -
     - time (current/historical/historical_series/forecast, default: current)
     - unit (metric/fahrenheit/scientific/celsius, default: celsius, f will be converted to c)
     - location (default: New Delhi)
-    - historical_date (if historical)
-    - historical_date_start (if time series historical)
-    - historical_date_end (if time series historical)
     - forecast_days (Of how many days forecast user need, default: 7)
 """
+
+#!!!Important note:
+#Access to/option of historical and forecast data is removed as it's not supported
+# in our subscription of weatherstack API!!!
 
 #Importing Prerequisites
 import requests
 import colours as c
 from colours import ran_col as col
-import time
-from datetime import date
 
-#Func
-def weather(hist=None, hist_start=None, hist_end=None, time_weather="current", unit="c", location="New Delhi", celsius=False):
-    # - 4 categories would be there, for historical, historical time series, forcast, current
-    if time_weather == "current":
-        current(unit=unit, location=location, celsius=celsius)
+#Key in var
+key = 'd02d6a1f40206cd47f8e0267f2a45e7c'
 
-    elif time_weather == "historical":
-        if hist is None:
-            return f"{c.acidic_red}Error: Past date or past time series date is not inserted properly{c.end}"
-
-        historical(hist=hist, unit=unit, location=location, celsius=celsius)
-
-    elif time_weather == "historical_series":
-        if hist_start is None:
-            return f"{c.acidic_red}Error: Past date or past time series date is not inserted properly{c.end}"
-
-        historical_series(hist_start=hist_start, hist_end=hist_end, unit=unit, location=location, celsius=celsius)
-
-    elif time_weather == "forecast":
-        forecast(unit=unit, location=location, celsius=celsius)
-
-    else:
-        return f"""{c.acidic_red}Error: 'time(current/historical/historical_series/forecast)' not specified{c.end}"""
+#---------------------------------------------------------------#
 
 
-#Function if user wants current weather
-def current(unit="c", location="New Delhi", celsius=False):
+#Function for current weather
+def current_weather(unit="c", location="New Delhi", celsius=False):
 
     url = "https://api.weatherstack.com/current?"
-    key = 'd02d6a1f40206cd47f8e0267f2a45e7c'
     params = {
         'query' : location,
         'access_key' : key,
@@ -91,32 +70,12 @@ Time Zone : {data['location']['timezone_id']}{c.end}
 """
     return output
 
-#---------------------------------------------------------------#
-
-#7 days ago date in var
-seven = time.time() - 604800
-seven = time.strftime('%Y-%m-%d', time.gmtime(seven))
-
-#Func for specific past date
-def historical(hist=seven, unit='f', location='New Delhi', celsius=False):
-    pass
-
-#---------------------------------------------------------------#
-
-#Func for specific past date
-def historical_series(hist_start=seven, hist_end=date.today(), unit='f', location='New Delhi', celsius=False):
-    pass
-
-#---------------------------------------------------------------#
-
-def forecast(unit='f', location='New Delhi', celsius=False):
-    pass
 
 #---------------------------------------------------------------#
 
 
 #Trial
 if __name__=="__main__":
-    return_val = (weather(location="Jodhpur", unit="f", celsius=True))
+    return_val = (current_weather(location="Jodhpur", unit="f", celsius=True))
     if return_val is not None:
         print(return_val)
